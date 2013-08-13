cb023f2bfc84f9a72e4bfbd85eb71ebed35f9ecf
Yuqian Li <spaceflyer@YBook-VBox.(none)>
2013-08-12 22:47:38 -0400

    Reimplement title and add some buttons to the toolbar.
    It seems that when buttons are added, the previous trick no
    longer works. So I invented my own "docked" trick.
    
    The buttons are now in default style and icon. Custom
    icon for removing completed item function is required
    in the future. It's also better to design custom style
    and 'more' button icon.

--- app/view/Main.js
+++ app/view/Main.js
@@ -11,13 +11,32 @@ Ext.define('DueList.view.Main', {
             {
             	docked: 'top',
             	xtype: 'toolbar',
-            	title: {
-            		title: 'Due List',
-            		style: {
-            			'text-align': 'left'
-            		}
+            	layout: {
+            		pack: 'end',
+            		type: 'hbox'
             	},
             	items: [
+            	        {
+            	        	xtype: 'title',
+            	        	title: 'Due List',
+            	        	docked: 'left'
+            	        },
+            	        {
+            	        	xtype: 'button',
+            	        	iconCls: 'refresh'
+            	        },
+            	        {
+            	        	xtype: 'button',
+            	        	iconCls: 'add'
+            	        },
+            	        {
+            	        	xtype: 'button',
+            	        	iconCls: 'delete'
+            	        },
+            	        {
+            	        	xtype: 'button',
+            	        	iconCls: 'more'
+            	        }
             	]
             }
         ]
